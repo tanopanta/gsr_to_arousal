3c45d33e6037c6e6eefa01958ba05a173abc81b4
tanopanta <rainstorm2525@gmail.com>
2018-11-04 03:52:26 +0900

    コメントを追加

--- gsr_test.ino
+++ gsr_test.ino
@@ -1,15 +1,12 @@
 // GSRセンサーで感情の縦軸をとる
 
-// センサーの2つの電極に指を入れていない状態＝抵抗マックスのときに、およそ2047(入力電圧のの半分)になる、ならない場合はねじを回せばよいらしい。
+// 皮膚コンダクタンスは自律神経系の覚醒の指標として使用できる
+// 根拠
+// Emotion and motivation I: defensive and appetitive reactions in picture processing.
+//https://www.researchgate.net/publication/10600214_Emotion_and_Motivation_I_Defensive_and_Appetitive_Reactions_in_Picture_Processing
+// 被引用数1924
 
-// https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2877802/
-// によると自律神経性覚醒はSCR(GSR)の Area under the curve(AUC)を指標として使える。
-//
-// サンプルコードは
-// scr_sf_auc(scr):
-//     scr = scr - min(scr)
-//     auc = mean(scr)
-//     return auc
+// センサーの2つの電極に指を入れていない状態＝抵抗マックスのときに、およそ2047(入力電圧のの半分)になる、ならない場合はねじを回せばよいらしい。
 
 //　https://www.birmingham.ac.uk/Documents/college-les/psych/saal/guide-electrodermal-activity.pdf
 //　によるとサンプリング周波数は1-5 samples per secondで良い。ただし感情の想起のタイミングが重要な場合は1msec
